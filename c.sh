4e63bd45171e934055e7c6f544efdde8305a3d69
David Valor <davidvalorwork@gmail.com>
2021-06-23 15:00:29 -0400

    update

--- c.sh
+++ c.sh
@@ -1,5 +1,4 @@
 echo ---- THANK YOU FOR USING COMMIT SCRIPT BY DAVID VALOR -----
-
 git config --global user.name 'David Valor'
 git config --global user.email 'davidvalorwork@gmail.com'
 git config credential.helper 'store --file ~/.credentialopencvpractice'
