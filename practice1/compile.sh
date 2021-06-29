b51694f7fd25943a09ed65990372fbb45bcb5715
David Valor <davidvalorwork@gmail.com>
2021-06-29 13:04:13 -0400

    update-practica1-finished

--- practice1/compile.sh
+++ practice1/compile.sh
@@ -1,3 +1,2 @@
-clear
 echo compliling...
 g++ -std=c++11 index.cpp -o program `pkg-config opencv4 --cflags --libs`
--- practice1/index.cpp
+++ practice1/index.cpp
@@ -1,3 +1,5 @@
+// FUNCTIONS THAT EVERYONE SHOULD NOW
+
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
@@ -8,15 +10,21 @@ using namespace std;
 int main(){
   string path = "../readImg/starry_night.jpg";
   Mat img = imread(path);
-  Mat imgGray, imgBlur, imgCanny;
+  Mat imgGray, imgBlur, imgCanny, imgDia, imgErode;
 
   cvtColor(img,imgGray,COLOR_BGR2GRAY);
   GaussianBlur(img,imgBlur,Size(7,7),5,0);
   Canny(imgBlur,imgCanny,50,150);
 
+  Mat kernel = getStructuringElement(MORPH_RECT,Size(5,5));
+  dilate(imgCanny, imgDia, kernel);
+  erode(imgDia, imgErode, kernel);
+
   imshow("Image",img);
   imshow("Image Gray",imgGray);
   imshow("Image Blur",imgBlur);
   imshow("Image Canny",imgCanny);
+  imshow("Image Dilate",imgDia);
+  imshow("Image Erode",imgErode);
   waitKey(0);
 }
